2e553b9107acbe8aba40d1c49b0797d692ae5351
Dantedod <danted2300@gmail.com>
2025-03-11 03:48:20 -0300

    make exportExercisies function

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -75,7 +75,7 @@ class MainController extends Controller
     echo '<hr>';
 
     foreach ($exercises as $exercise) {
-      echo '<h2><small>' . str_pad($exercise['exercise_number'], 2, '0', STR_PAD_LEFT) . ' >> </small>' . $exercise['exercise'] . '</h2>';
+      echo '<h2><small>' . $exercise['exercise_number']  . ' >> </small>' . $exercise['exercise'] . '</h2>';
     }
 
     //solution
@@ -83,12 +83,37 @@ class MainController extends Controller
     echo '<small> Solucoes</small><br>';
 
     foreach ($exercises as $exercise) {
-      echo '<small>' . str_pad($exercise['exercise_number'], 2, '0', STR_PAD_LEFT) . ' >> ' . $exercise['exercise'] . $exercise['sollution'] . '</small><br>';
+      echo '<small>'  . $exercise['exercise_number']  . ' >> ' . $exercise['exercise'] . $exercise['sollution'] . '</small><br>';
     }
   }
   public function exportExercisies()
   {
-    echo "Exportar exercicios para um arquivo de texto";
+    //here check if exists exercises in session
+    if (!session()->has('exercises')) {
+      return redirect()->route('home');
+    };
+
+    //recebe os exercicios
+    $exercises = session('exercises');
+
+    //create file to download with exercises
+    $filename = 'exercises_' . env('APP_NAME') . '_' . date('YmdHis') . '.txt';
+
+    $content = '';
+    foreach ($exercises as $exercise) {
+      $content .= $exercise['exercise_number'] . ' > ' . $exercise['exercise'] . "\n";
+    }
+
+    //solutions
+    $content .= "\n";
+    $content .= 'Solucoes' .  "\n" . str_repeat('-', 20) . "\n";
+    foreach ($exercises as $exercise) {
+      $content .= $exercise['exercise_number'] . ' = ' . $exercise['sollution'] . "\n";
+    }
+
+    return response($content)
+      ->header('Content-Type', 'text/plain')
+      ->header('Content-Disposition', 'attachment; filename= "' . $filename . '"');
   }
   private function generateExercisie($index, $opreations, $min, $max): array
   {
@@ -130,7 +155,7 @@ class MainController extends Controller
     return
       [
         'operation' => $opreation,
-        'exercise_number' => $index,
+        'exercise_number' => str_pad($index, 2, '0', STR_PAD_LEFT),
         'exercise' => $exercise,
         'sollution' => " $sollution "
       ];
--- resources/views/operations.blade.php
+++ resources/views/operations.blade.php
@@ -29,7 +29,7 @@
 
             @foreach ($exercisies as $exercise)
                 <div class="col-3 display-6 mb-3">
-                    <span class="badge bg-dark">{{ str_pad($exercise['exercise_number'], 2, '0', STR_PAD_LEFT) }}</span>
+                    <span class="badge bg-dark">{{ $exercise['exercise_number'] }}</span>
                     <span>{{ $exercise['exercise'] }}</span>
 
                 </div>
@@ -48,8 +48,8 @@
                 <a href="{{ route('home') }}" class="btn btn-primary px-5">VOLTAR</a>
             </div>
             <div class="col text-end">
-                <a href="{{ route('printExercisies') }}" class="btn btn-secondary px-5">DESCARREGAR EXERCÍCIOS</a>
-                <a href="{{ route('exportExercisies') }}" class="btn btn-secondary px-5">IMPRIMIR EXERCÍCIOS</a>
+                <a href="{{ route('exportExercisies') }}" class="btn btn-secondary px-5">DESCARREGAR EXERCÍCIOS</a>
+                <a href="{{ route('printExercisies') }}" class="btn btn-secondary px-5">IMPRIMIR EXERCÍCIOS</a>
             </div>
         </div>
     </div>
